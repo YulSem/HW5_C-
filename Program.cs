dbaea665f2ca2ea1d1cb3a0b8db57d6529de9943
Yuliya <you@example.com>
2023-08-07 13:04:42 +0300

    Задача 34

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int [] array= new int [900];
+// int [] array= new int [900];
 
-int count=0;
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(100,1000);
-    System.Console.Write(array[i]+ " "); 
+// int count=0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(100,1000);
+//     System.Console.Write(array[i]+ " "); 
 
-    if (array[i]%2==0)
-    {
-        count++;
-    }
-}
-System.Console.WriteLine("Кол-во равно "+ count);
+//     if (array[i]%2==0)
+//     {
+//         count++;
+//     }
+// }
+// System.Console.WriteLine("Кол-во равно "+ count);
